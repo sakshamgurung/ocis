7e60199a3f2b376ae5fe0d60acd3756e8494ea65
Willy Kloucek <wkloucek@owncloud.com>
2021-08-13 15:45:15 +0200

    remove users script

--- deployments/examples/ocis_oc10_keycloak_ldap_migration/config/oc10/10-ensure-users.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# ensure users exist, because first login in oCIS causes trouble...
-curl http://localhost:8080/remote.php/webdav -u marie:radioactivity
-curl http://localhost:8080/remote.php/webdav -u einstein:relativity
-curl http://localhost:8080/remote.php/webdav -u richard:superfluidity
-
-true
